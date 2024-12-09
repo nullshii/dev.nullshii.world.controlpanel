5bca7f4c0ebf2f8e9ee611e8ef264bcd94282a99
nullshii <null>
2024-12-09 11:02:40 +0400

    Added autodetect function to ControlPanelMountInspector.cs

--- Editor/Script/ControlPanelMountInspector.cs
+++ Editor/Script/ControlPanelMountInspector.cs
@@ -1,3 +1,4 @@
+using System;
 using ControlPanel.Script;
 using UnityEditor;
 using UnityEngine;
@@ -7,6 +8,13 @@ namespace ControlPanel.Editor.Script
     [CustomEditor(typeof(ControlPanelMount), false)]
     public class ControlPanelMountInspector : UnityEditor.Editor
     {
+        private SerializedProperty _serializedControlPanel;
+
+        private void OnEnable()
+        {
+            _serializedControlPanel = serializedObject.FindProperty("_controlPanel");
+        }
+
         public override void OnInspectorGUI()
         {
             DrawDefaultInspector();
@@ -15,6 +23,24 @@ namespace ControlPanel.Editor.Script
 
             EditorGUILayout.Space();
 
+            if (GUILayout.Button("Auto detect control panel"))
+            {
+                var controlPanels = FindObjectsOfType<ControlPanel.Script.ControlPanel>();
+
+                switch (controlPanels.Length)
+                {
+                    case 0:
+                        Debug.LogWarning("Control panel not found.");
+                        return;
+                    case > 1:
+                        Debug.LogWarning("Multiple control panels found in scene! Consider using 1 panel and many mount points.");
+                        return;
+                }
+
+                ControlPanel.Script.ControlPanel controlPanel = controlPanels[0]; 
+                _serializedControlPanel.objectReferenceValue = controlPanel;
+            }
+            
             if (GUILayout.Button("Reset control panel position to this mount"))
             {
                 var controlPanelMount = target as ControlPanelMount;
